efda28fd17c1489ddac442933af5507ef3f49709
Rob Seaver <rob.seaver@gmail.com>
2025-04-14 18:56:41 -0500

    test: get app controller test working-ish

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,23 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { describe, beforeEach, it, expect } from 'vitest';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [{
+        provide: AppService,
+        useValue: {
+          getHello: vi.fn()
+        }
+      }]
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+
+  it('should return "Hello World!"', () => {
+    vi.spyOn(appService, 'getHello').mockReturnValue('Hello World!');
+    expect(appController.getHello()).toBe('Hello World!');
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // For whatever reason, the test only runs for this controller when the we @Inject the AppService.
+  // There's a use case here: https://docs.nestjs.com/fundamentals/custom-providers, but that doesn't apply
+  constructor(@Inject(AppService) private appService: AppService) {}
 
   @Get()
   getHello(): string {
