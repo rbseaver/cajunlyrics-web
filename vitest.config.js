19a1b5df560c98af6e6373608d8060129fb63b81
Rob Seaver <rob.seaver@gmail.com>
2025-04-14 18:55:25 -0500

    chore: update coverage reporters

--- vitest.config.js
+++ vitest.config.js
@@ -1,15 +1,14 @@
 /// <reference types="vitest/config" />
 
-import { defineConfig, configDefaults } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
     include: ['**/*.spec.ts'],
-    globalSetup: './test-setup.ts',
     coverage: {
-      reporter: ['text', 'json', 'html'],
+      reporter: ['lcov', 'cobertura'],
       exclude: [
         '**/__tests__/**',
         '**/__mocks__/**',
