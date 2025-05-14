2dd6331f93effda9509d09e824c8be7ac2bc378a
Rob Seaver <rob.seaver@gmail.com>
2025-05-14 14:17:53 -0500

    chore: initial files for lyrics retrieval

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VersionController } from './version/version.controller';
 import { VersionService } from './version/version.service';
+import { LyricsController } from './lyrics/lyrics.controller';
+import { LyricsService } from './lyrics/lyrics.service';
 
 @Module({
   imports: [],
-  controllers: [VersionController],
-  providers: [VersionService],
+  controllers: [VersionController, LyricsController],
+  providers: [VersionService, LyricsService],
 })
 export class AppModule {}
---
+++ src/lyrics/lyrics.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('lyrics')
+export class LyricsController {}
