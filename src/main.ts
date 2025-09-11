17d2c88738ab399436026a31d0627e7e35b1c341
Rob Seaver <rob.seaver@gmail.com>
2025-09-11 12:05:17 -0500

    feat: add Swagger to document endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('CajunLyricsWeb API')
+    .setDescription('API to fetch lyrics for songs')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
   
   await app.listen(process.env.PORT ?? 3000);
-
 }
 bootstrap();
--- src/version/version.controller.ts
+++ src/version/version.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { VersionService, BuildInformation } from './version.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Version')
 @Controller('version')
 export class VersionController {
-  constructor(@Inject(VersionService) private readonly versionService: VersionService) {}
+  constructor(@Inject(VersionService) private readonly versionService: VersionService) { }
   /**
    * Get the version of the application
    * @returns BuildInformation Object containing the version
    */
   @Get()
+  @ApiOperation({ summary: 'Get the version of the application' })
+  @ApiResponse({ status: 200, description: 'The version of the application', type: Object })
   getVersion(): BuildInformation {
     return this.versionService.getVersion();
   }
